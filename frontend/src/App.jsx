3d0f905522a2a544ddb028a9a81e8e6435da5b3d
Muhammad Usman <mu801710@gmail.com>
2026-04-10 19:23:22 +0500

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,7 @@ import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-      <Header />
       <Routes />
-      <Footer />
     </>
   )
 }
--- frontend/src/pages/Frontend/index.jsx
+++ frontend/src/pages/Frontend/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
@@ -7,11 +9,13 @@ import Contact from './Contact'
 const Frontend = () => {
     return (
         <main>
+            <Header />
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="contact" element={<Contact />} />
             </Routes>
+            <Footer />
         </main>
     )
 }
