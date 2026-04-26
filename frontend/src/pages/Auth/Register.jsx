9fec2a0210301b2eb6da95a07c0c3773bed4d324
Muhammad Usman <mu801710@gmail.com>
2026-04-26 10:13:05 +0500

    login

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -56,7 +56,7 @@ const Register = () => {
             form.resetFields()
 
             setTimeout(() => {
-                navigate('/login')
+                navigate('/auth/login')
             }, 1000)
 
         } catch (err) {
